bf66944073093be801bbceb04c6f6f370ade7122
tvcong0999 <tvcong0999@gmail.com>
2021-01-12 14:57:05 +0700

    update layout main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -8,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   constructor() { }
+  time = new Date();
+  timer;
+  dateObj = new Date();
 
   ngOnInit(): void {
+    this.timer = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
   }
 
+   clock() {
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    $("#sec").css("width",s*300/60+"px");
+    $("#min").css("width",m*300/60+"px");
+    $("#hour").css("width",h*300/24+"px");
+    $("#txthour").html(h);
+    $("#txtmin").html(m);
+    $("#txtsec").html(s);
+
+    setTimeout("clock()", 1000);
+}
 }
